d66aea2c32bf4802c8ee93a8357ba46273e87aaa
Dimitris Gerasimou <gerasimou@ece.auth.gr>
2023-03-04 04:05:29 +0200

    Added clickable blocks

--- src/config.h
+++ src/config.h
@@ -1,9 +1,9 @@
 #ifndef CONFIGURATION
 #define CONFIGURATION
 
-#define CLICKABLE_BLOCKS 0
+#define CLICKABLE_BLOCKS 1
 #define LEADING_DELIMITER 0
-#define DELIMITER ""
+#define DELIMITER "  "
 
 #define SCRIPTPATH(SCRIPTNAME) "$HOME/.local/bin/dwmblocks/" #SCRIPTNAME
 
@@ -11,12 +11,13 @@ const Block blocks[] = {
 	/*Command                  Update Interval   Update Signal */
 	{ SCRIPTPATH("volume"),    0,                10 },
 	{ SCRIPTPATH("memory"),    6,                0  },
-	{ SCRIPTPATH("internet"),  5,                1  },
-	{ SCRIPTPATH("battery"),   5,                2  },
 	{ SCRIPTPATH("keyboard"),  0,                3  },
 	{ SCRIPTPATH("kernel"),    360,              4  },
 	{ SCRIPTPATH("date"),      300,              6  },
 	{ SCRIPTPATH("time"),      1,                5  },
+	{ SCRIPTPATH("battery"),   5,                2  },
+	{ SCRIPTPATH("internet"),  5,                1  },
+	{ SCRIPTPATH("spacer"),    0,                11 },
 };
 
 #endif /* ifndef CONFIGURATION */
