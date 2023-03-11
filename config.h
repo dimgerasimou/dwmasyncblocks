9d83543aa0626f594178ff4d2fd773ce91a2b467
Dimitris Gerasimou <gerasimou@ece.auth.gr>
2023-03-11 05:43:59 +0200

    Added spacer for fix

--- config.h
+++ config.h
@@ -10,13 +10,13 @@
 const Block blocks[] = {
 	/*Command                  Update Interval   Update Signal */
 	{ SCRIPTPATH("volume"),    0,                10 },
-	{ SCRIPTPATH("memory"),    6,                0  },
+	{ SCRIPTPATH("spacer-0"),  0,                13 },
+	{ SCRIPTPATH("memory"),    6,                12 },
 	{ SCRIPTPATH("keyboard"),  0,                3  },
 	{ SCRIPTPATH("kernel"),    360,              4  },
 	{ SCRIPTPATH("date"),      300,              6  },
 	{ SCRIPTPATH("time"),      1,                5  },
 	{ SCRIPTPATH("battery"),   5,                2  },
-	{ SCRIPTPATH("spacer-0"),  0,                11 },
 	{ SCRIPTPATH("internet"),  5,                1  },
 	{ SCRIPTPATH("spacer"),    0,                11 }
 };
