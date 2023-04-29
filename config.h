62675c8ebb980cc322d18d0f9b3bec3d419ed483
Dimitris Gerasimou <gerasimou@ece.auth.gr>
2023-04-29 20:22:09 +0300

    Removed delimiters

--- config.h
+++ config.h
@@ -2,8 +2,8 @@
 #define CONFIGURATION
 
 #define CLICKABLE_BLOCKS 1
-#define LEADING_DELIMITER 1
-#define DELIMITER "  "
+#define LEADING_DELIMITER 0
+#define DELIMITER ""
 
 #define SCRIPTPATH(SCRIPTNAME) "$HOME/.local/bin/dwmblocks/" #SCRIPTNAME
 
@@ -15,8 +15,8 @@ const Block blocks[] = {
 	{ SCRIPTPATH("kernel"),    360,              4  },
 	{ SCRIPTPATH("date"),      300,              6  },
 	{ SCRIPTPATH("time"),      1,                5  },
-	{ SCRIPTPATH("battery"),   5,                2  },
 	{ SCRIPTPATH("internet"),  5,                1  },
+	{ SCRIPTPATH("battery"),   5,                2  },
 	{ SCRIPTPATH("bluetooth"), 5,                15 },
 	{ SCRIPTPATH("power"),     0,                14 },
 	{ SCRIPTPATH("spacer"),    0,                11 }
